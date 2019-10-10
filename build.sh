8168fd2e96854335e946b888f14f1965398fa6ad
Your Name <you@example.com>
2019-10-10 10:43:51 +0800

    update for 0.14.x

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./build-package.sh -k feat/0.11.2 -p "build_kong:7"
-./build-package.sh -k feat/0.11.2 -p "build_kong:6"
+./build-package.sh -k feat/0.14.0 -p "build_kong:7"
+./build-package.sh -k feat/0.14.0 -p "build_kong:6"
