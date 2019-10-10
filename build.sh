fee07e2e28f76f6c88186c9f723a822f3f781cc4
Your Name <you@example.com>
2019-10-10 10:51:37 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./build-package.sh -k feat/0.14.0 -p "build_kong:7"
-./build-package.sh -k feat/0.14.0 -p "build_kong:6"
+./build-package.sh -k feat/0.14.1 -p "icyboy/build_kong:0.14.x-centos7"
+./build-package.sh -k feat/0.14.1 -p "icyboy/build_kong:0.14.x-centos6"
